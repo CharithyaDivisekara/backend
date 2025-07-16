14a0d16751465756786fd9c701d1a73b1fa534e7
DMRSCK DIvisekara <10charithyaskd@gmail.com>
2025-07-16 09:03:01 +0530

    clean up ProductRepository interface

--- src/main/java/com/example/backend/ProductRepository.java
+++ src/main/java/com/example/backend/ProductRepository.java
@@ -3,5 +3,5 @@ package com.example.backend;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    // You can add custom queries if needed
+   
 }
